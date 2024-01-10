b4dc3d3fe2f0f99658215c01415f8afdcf1a4a7b
Sekhar Babu Patnam <babuthetrainer@gmail.com>
2024-01-10 07:27:40 +0530

    Cloud Config server & client demos

---
+++ sn-cloud-config-client/src/main/java/com/servicenow/configclient/SnCloudConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.servicenow.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnCloudConfigClientApplication.class, args);
+	}
+
+}
---
+++ sn-cloud-config-client/src/test/java/com/servicenow/configclient/SnCloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicenow.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnCloudConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
